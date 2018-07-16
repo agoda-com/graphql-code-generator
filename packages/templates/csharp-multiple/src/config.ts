707ac1b896cf00472c18d672ae365acf486bdd6f
AGODA\jlaszlo <jeno.laszlo@agoda.com>
2018-07-16 17:33:10 +0700

    moved templates to csharp dir

--- packages/templates/csharp-multiple/src/config.ts
+++ packages/templates/csharp-multiple/src/config.ts
@@ -1,10 +1,10 @@
 import * as type from './type.handlebars';
-import * as enumTemplate from '../../typescript/src/enum.handlebars';
+import * as enumTemplate from './enum.handlebars';
 import * as scalar from './scalar.handlebars';
 import * as union from './union.handlebars';
 import * as operation from './operation.handlebars';
 import * as fragment from './fragment.handlebars';
-import * as selectionSet from '../../typescript/src/selection-set.handlebars';
+import * as selectionSet from './selection-set.handlebars';
 import * as fragments from './fragments.handlebars';
 import { EInputType, GeneratorConfig } from 'graphql-codegen-core';
 import { getType } from './helpers/get-type';
